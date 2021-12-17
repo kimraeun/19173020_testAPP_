bfae130fc9336b48d38f6a5e26a2a9c6e81d10fd
raeun <1013cc@naver.com>
2021-12-17 20:51:05 +0900

    upload 11app

---
+++ app/src/main/java/com/example/a11_app/Movie.java
@@ -0,0 +1,23 @@
+package com.example.a11_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiInten;
+    String audichange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+
+}
